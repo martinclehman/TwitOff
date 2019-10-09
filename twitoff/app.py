1c58290295d761693688045af8f4ec893b0f1302
martinclehman <martinclehman@users.noreply.github.com>
2019-10-09 04:33:15 -0400

    update to M3 notes

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-from .models import DB
+from decouple import config
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
 
 # Unix/Mac
 # 'sqlite:////absolute/path/to/db.sqlite3'
 # Windows
 # 'sqlite:///C:\\Users\\bruno\\OneDrive\\Desktop\\TwitOff\\twitoff\\db.sqlite3'
+
+load_dotenv()
 
 def create_app():
     """Create and configure an instance of the Flask application"""
@@ -14,6 +19,24 @@ def create_app():
 
     @app.route('/')
     def root():
-        return("Welcome to TwitOff!")
+        return render_template('base.html', title='TwitOff!', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.username==name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,message=message)
 
     return app
+
+
+    def compare_users():
+        pass
--- twitoff/models.py
+++ twitoff/models.py
@@ -7,8 +7,14 @@ class User(DB.Model):
     """Twitter users that we query and store historical tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(15), unique=True, nullable=False)
+    followers = DB.Column(DB.BigInteger, nullable=False)
+    # Tweets IDs are ordinal ints, so we can fetch most recent tweets
+    newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
 class Tweets(DB.Model):
     """Stores tweets"""
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(280))
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
