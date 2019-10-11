466309b11d10790bc735b271654d1f83da063327
martinclehman <martinclehman@users.noreply.github.com>
2019-10-10 22:44:35 -0400

    change timeout syntax and add sqlachemy_track_modifications line

--- twitoff/app.py
+++ twitoff/app.py
@@ -16,6 +16,7 @@ def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
